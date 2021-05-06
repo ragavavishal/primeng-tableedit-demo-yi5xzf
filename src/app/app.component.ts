df6cf6684fea6af9f0d1ab544f3f9f39f984a25c
ragavavishal <ragavavishal@gmail.com>
2021-05-06 08:25:20 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,135 @@
-import {Component} from '@angular/core';
-import { ProductService } from './productservice';
-import { Product } from './product';
-import { FilterUtils } from 'primeng/utils';
-import { LazyLoadEvent } from 'primeng/api';
-import { SelectItem } from 'primeng/api';
-import {MessageService} from 'primeng/api';
+import { Component } from "@angular/core";
+import { ProductService } from "./productservice";
+import { Product } from "./product";
+import { FilterUtils } from "primeng/utils";
+import { LazyLoadEvent } from "primeng/api";
+import { SelectItem } from "primeng/api";
+import { MessageService } from "primeng/api";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
   providers: [MessageService],
-  styles: [`
+  styles: [
+    `
       :host ::ng-deep .p-cell-editing {
-          padding-top: 0 !important;
-          padding-bottom: 0 !important;
+        padding-top: 0 !important;
+        padding-bottom: 0 !important;
       }
-  `]
+    `
+  ]
 })
-export class AppComponent { 
-    products1: Product[];
+export class AppComponent {
+  load: boolean = false;
+  products1: Product[];
 
-    products2: Product[];
+  products2: Product[];
 
-    statuses: SelectItem[];
+  statuses: SelectItem[];
 
-    clonedProducts: { [s: string]: Product; } = {};
+  clonedProducts: { [s: string]: Product } = {};
 
-    constructor(private productService: ProductService, private messageService: MessageService) { }
+  editForm: FormGroup;
 
-    ngOnInit() {
-        this.productService.getProductsSmall().then(data => this.products1 = data);
-        this.productService.getProductsSmall().then(data => this.products2 = data);
+  constructor(
+    private productService: ProductService,
+    private messageService: MessageService,
+    private _fb: FormBuilder
+  ) {}
 
-        this.statuses = [{label: 'In Stock', value: 'INSTOCK'},{label: 'Low Stock', value: 'LOWSTOCK'},{label: 'Out of Stock', value: 'OUTOFSTOCK'}]
+  ngOnInit() {
+    this.productService.getProductsSmall().then(data => {
+      this.products2 = data;
+
+      this.products2 = this.products2.slice(0, 5);
+
+      this.editForm = this._fb.group({
+        fields: this._fb.array([])
+      });
+      this.products2.map((item, index) => {
+        this.fieldsArray.push(this.addFieldsGroup(item));
+      });
+      this.load = true;
+    });
+
+    this.statuses = [
+      { label: "In Stock", value: "INSTOCK" },
+      { label: "Low Stock", value: "LOWSTOCK" },
+      { label: "Out of Stock", value: "OUTOFSTOCK" }
+    ];
+  }
+
+  addFieldsGroup(item: Product) {
+    return this._fb.group({
+      id: [item.id, Validators.required],
+      code: [item.code, Validators.required],
+      name: [item.name, Validators.required],
+      inventoryStatus: [item.inventoryStatus, Validators.required],
+      price: [item.price, Validators.required]
+    });
+  }
+
+  get fieldsArray() {
+    return <FormArray>this.editForm.get("fields");
+  }
+
+  addNewRow() {
+    let item: Product = {
+      id: "0",
+      code: "",
+      name: "",
+      inventoryStatus: "",
+      price: null
+    };
+    this.products2.push(item);
+    this.fieldsArray.push(this.addFieldsGroup(item));
+  }
+
+  removeFields(index) {
+    this.load = false;
+    this.fieldsArray.removeAt(index);
+    this.editForm.updateValueAndValidity();
+    this.products2.splice(index, 1);
+    this.load = true;
+  }
+
+  onSubmit() {
+    console.log("Form Value", this.editForm.value);
+    console.log("Formarray Controls", this.fieldsArray.controls);
+    console.log("Products Var Value", this.products2);
+    const filtered = this.fieldsArray.value.filter((item, index) => {
+      if (this.fieldsArray.controls[index].dirty) {
+        return true;
+      }
+      return false;
+    });
+    console.log("Edited Data", filtered);
+  }
+
+  onRowEditInit(product: Product) {
+    this.clonedProducts[product.id] = { ...product };
+  }
+
+  onRowEditSave(product: Product) {
+    if (product.price > 0) {
+      delete this.clonedProducts[product.id];
+      this.messageService.add({
+        severity: "success",
+        summary: "Success",
+        detail: "Product is updated"
+      });
+    } else {
+      this.messageService.add({
+        severity: "error",
+        summary: "Error",
+        detail: "Invalid Price"
+      });
     }
+  }
 
-    onRowEditInit(product: Product) {
-        this.clonedProducts[product.id] = {...product};
-    }
-
-    onRowEditSave(product: Product) {
-        if (product.price > 0) {
-            delete this.clonedProducts[product.id];
-            this.messageService.add({severity:'success', summary: 'Success', detail:'Product is updated'});
-        }  
-        else {
-            this.messageService.add({severity:'error', summary: 'Error', detail:'Invalid Price'});
-        }
-    }
-
-    onRowEditCancel(product: Product, index: number) {
-        this.products2[index] = this.clonedProducts[product.id];
-        delete this.products2[product.id];
-    }
+  onRowEditCancel(product: Product, index: number) {
+    this.products2[index] = this.clonedProducts[product.id];
+    delete this.products2[product.id];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppComponent }   from './app.component';
-import { ProductService } from './productservice';
+import { AppComponent } from "./app.component";
+import { ProductService } from "./productservice";
 
-import {TableModule} from 'primeng/table';
-import {ToastModule} from 'primeng/toast';
-import {CalendarModule} from 'primeng/calendar';
-import {SliderModule} from 'primeng/slider';
-import {MultiSelectModule} from 'primeng/multiselect';
-import {ContextMenuModule} from 'primeng/contextmenu';
-import {DialogModule} from 'primeng/dialog';
-import {ButtonModule} from 'primeng/button';
-import {DropdownModule} from 'primeng/dropdown';
-import {ProgressBarModule} from 'primeng/progressbar';
-import {InputTextModule} from 'primeng/inputtext';
+import { TableModule } from "primeng/table";
+import { ToastModule } from "primeng/toast";
+import { CalendarModule } from "primeng/calendar";
+import { SliderModule } from "primeng/slider";
+import { MultiSelectModule } from "primeng/multiselect";
+import { ContextMenuModule } from "primeng/contextmenu";
+import { DialogModule } from "primeng/dialog";
+import { ButtonModule } from "primeng/button";
+import { DropdownModule } from "primeng/dropdown";
+import { ProgressBarModule } from "primeng/progressbar";
+import { InputTextModule } from "primeng/inputtext";
 
 @NgModule({
   imports: [
@@ -25,21 +25,21 @@ import {InputTextModule} from 'primeng/inputtext';
     BrowserAnimationsModule,
     TableModule,
     CalendarModule,
-		SliderModule,
-		DialogModule,
-		MultiSelectModule,
-		ContextMenuModule,
-		DropdownModule,
-		ButtonModule,
-		ToastModule,
+    SliderModule,
+    DialogModule,
+    MultiSelectModule,
+    ContextMenuModule,
+    DropdownModule,
+    ButtonModule,
+    ToastModule,
     InputTextModule,
     ProgressBarModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
   providers: [ProductService]
 })
-
-export class AppModule { }
+export class AppModule {}
